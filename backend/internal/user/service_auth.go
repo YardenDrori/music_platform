b20efb3cb596e0476c9dd0606046fe57aedaeb75
YardenDrori <yarden.drori.yd@gmail.com>
2026-05-16 20:53:38 +0300

    added email verif
    but imma call it a night for today so cya to whomever might be reading
    this, have an absofuckinglutely wonderful day :)

--- backend/internal/user/service_auth.go
+++ backend/internal/user/service_auth.go
@@ -2,13 +2,23 @@ package user
 
 import (
 	"context"
-	"fmt"
 	"strings"
 )
 
-func (s *userService) Register(ctx context.Context, req RegisterRequest) (*User, error) {
+func (s *userService) Register(ctx context.Context, req *RegisterRequest) (*User, error) {
+	if before, after, ok := strings.Cut(
+		req.Email,
+		"@",
+	); !ok || after == "" || before == "" ||
+		strings.ContainsRune(after, '@') || !strings.Contains(after, ".") {
+		return nil, &ErrBadRequest{Message: "invalid email"}
+	}
+
+	// var userName = strings.Clone(req.Email)
+
 	panic("")
 }
-func (s *userService) Login(ctx context.Context, req LoginRequest) (*User, error) {
+
+func (s *userService) Login(ctx context.Context, req *LoginRequest) (*User, error) {
 	panic("")
 }
