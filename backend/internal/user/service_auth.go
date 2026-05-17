d0c0cfbc032f92023b698f06f1bc0467793d6c57
YardenDrori <yarden.drori.yd@gmail.com>
2026-05-17 16:20:49 +0300

    finished registration code

--- backend/internal/user/service_auth.go
+++ backend/internal/user/service_auth.go
@@ -2,10 +2,22 @@ package user
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"strings"
+	"time"
+	"unicode/utf8"
+
+	"github.com/google/uuid"
 )
 
 func (s *userService) Register(ctx context.Context, req *RegisterRequest) (*User, error) {
+	if req.Email == "" || req.FirstName == "" || req.LastName == "" || req.Password == "" ||
+		req.UserName == "" {
+		return nil, &ErrBadRequest{Message: "missing fields"}
+	}
+
+	//email verif
 	if before, after, ok := strings.Cut(
 		req.Email,
 		"@",
@@ -14,9 +26,52 @@ func (s *userService) Register(ctx context.Context, req *RegisterRequest) (*User
 		return nil, &ErrBadRequest{Message: "invalid email"}
 	}
 
-	// var userName = strings.Clone(req.Email)
+	if utf8.RuneCountInString(req.Password) < 8 {
+		return nil, &ErrBadRequest{Message: "password too short"}
+	}
 
-	panic("")
+	//verify email and username are available
+	_, errEmail := s.repo.FindByEmail(ctx, req.Email)
+	switch {
+	case errEmail == nil:
+		return nil, &ErrBadRequest{Message: "email unavailable"}
+	case errors.Is(errEmail, ErrNotFound):
+		break
+	default:
+		return nil, fmt.Errorf("checking email availability: %w", errEmail)
+	}
+	_, errUsername := s.repo.FindByUsername(ctx, req.UserName)
+	switch {
+	case errUsername == nil:
+		return nil, &ErrBadRequest{Message: "username unavailable"}
+	case errors.Is(errUsername, ErrNotFound):
+		break
+	default:
+		return nil, fmt.Errorf("checking username availability: %w", errUsername)
+	}
+
+	passwordHash := s.hasher.hashPassword(req.Password)
+
+	newUser := User{
+		ID:           uuid.New(),
+		Email:        req.Email,
+		Username:     req.UserName,
+		FirstName:    req.FirstName,
+		LastName:     req.LastName,
+		PasswordHash: passwordHash,
+		CreatedAt:    time.Now().UTC(),
+		Active:       true,
+	}
+
+	repoErr := s.repo.Create(ctx, &newUser)
+	switch {
+	case repoErr == nil:
+		return &newUser, nil
+	case errors.Is(repoErr, ErrConflict):
+		return nil, &ErrBadRequest{Message: "email or username unavailable"}
+	default:
+		return nil, fmt.Errorf("attempting to create new user: %w", repoErr)
+	}
 }
 
 func (s *userService) Login(ctx context.Context, req *LoginRequest) (*User, error) {
