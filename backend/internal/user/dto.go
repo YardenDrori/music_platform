5fe828e8754490da1c89eab154fd10a6656386f4
YardenDrori <yarden.drori.yd@gmail.com>
2026-05-18 10:34:23 +0300

    added loginresponse

--- backend/internal/user/dto.go
+++ backend/internal/user/dto.go
@@ -8,8 +8,15 @@ type RegisterRequest struct {
 	LastName  string
 	Password  string
 }
+
 type LoginRequest struct {
 	Email    *string
 	UserName *string
 	Password string
+}
+
+type LoginResponse struct {
+	User         *User
+	RefreshToken string
+	AccessToken  string
 }
