cc3500e61c4956a8c143c0f904b7e980756d42b3
YardenDrori <yarden.drori.yd@gmail.com>
2026-05-19 11:14:46 +0300

    implemented metod generating an access token

--- backend/internal/user/tokens.go
+++ backend/internal/user/tokens.go
@@ -3,46 +3,67 @@ package user
 import (
 	"context"
 	"fmt"
-	"go/token"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/google/uuid"
 )
 
 type Claims struct {
 	jwt.RegisteredClaims
-	ID       uuid.UUID
-	Username string
-	Email    string
 }
 
 type Tokenizer interface {
 	ValidateAccessToken(ctx context.Context, token string) (Claims, error)
 }
 
-type JWTTokenizer struct {
-	signingKey []byte
+type JWTTokenizerHS256 struct {
+	signingKey         []byte
+	accessTokenDurSex  time.Duration
+	refreshTokenDurSec time.Duration
 }
 
-func NewJwtTokenizer(signingkey []byte) Tokenizer {
-	return &JWTTokenizer{signingKey: signingkey}
+func NewJwtTokenizer(
+	signingkey []byte,
+	accessTokenDurSex time.Duration,
+	refreshTokenDurSec time.Duration,
+) Tokenizer {
+	return &JWTTokenizerHS256{
+		signingKey:         signingkey,
+		accessTokenDurSex:  accessTokenDurSex,
+		refreshTokenDurSec: refreshTokenDurSec,
+	}
 }
 
-func (t *JWTTokenizer) newAccess(
+func (t *JWTTokenizerHS256) newAccess(
 	ctx context.Context,
 	user *User,
-) (string, error) {
-	panic("")
+) (*string, error) {
+	expTime := time.Now().UTC().Add(t.accessTokenDurSex)
+
+	claims := Claims{
+		jwt.RegisteredClaims{
+			Subject:   user.ID.String(),
+			IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+			ExpiresAt: jwt.NewNumericDate(expTime),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	finalizedToken, err := token.SignedString(t.signingKey)
+	if err != nil {
+		return nil, fmt.Errorf("generating new access token: %w", err)
+	}
+	return &finalizedToken, nil
 }
 
-func (t *JWTTokenizer) generateTokenPair(
+func (t *JWTTokenizerHS256) generateTokenPair(
 	ctx context.Context,
 	user *User,
 ) (string, string, error) {
 	panic("")
 }
 
-func (t *JWTTokenizer) ValidateAccessToken(ctx context.Context, token string) (Claims, error) {
+func (t *JWTTokenizerHS256) ValidateAccessToken(ctx context.Context, token string) (Claims, error) {
 	panic("")
 }
