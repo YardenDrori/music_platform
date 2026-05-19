173d4db0a02290bd2075f5101707bdc467f9256a
YardenDrori <yarden.drori.yd@gmail.com>
2026-05-19 18:33:44 +0300

    added error for expired tokens

--- backend/internal/user/errors.go
+++ backend/internal/user/errors.go
@@ -10,6 +10,8 @@ var ErrConflict = errors.New("conflict")
 
 var ErrUnauthorized = errors.New("incorrect username or password")
 
+var ErrExpiredToken = errors.New("token expired")
+
 type ErrBadRequest struct {
 	Message string
 }
