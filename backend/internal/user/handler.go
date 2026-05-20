1a36bd51d1956d7591979dc1c34b7027f425a890
YardenDrori <yarden.drori.yd@gmail.com>
2026-05-20 10:18:03 +0300

    write error method for handler and register handler

--- backend/internal/user/handler.go
+++ backend/internal/user/handler.go
@@ -1,7 +1,9 @@
 package user
 
 import (
-	"fmt"
+	"encoding/json"
+	"errors"
+	"log/slog"
 	"net/http"
 )
 
@@ -9,6 +11,33 @@ type handler struct {
 	service AuthService
 }
 
+func writeError(w http.ResponseWriter, status int, message string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(map[string]string{"error": message})
+}
+
 func (h *handler) Register(w http.ResponseWriter, r *http.Request) {
-	panic("")
+	var req registerRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		writeError(w, http.StatusBadRequest, "invalid request body")
+		return
+	}
+
+	user, err := h.service.Register(r.Context(), &req)
+	if err != nil {
+		if err, ok := errors.AsType[*ErrBadRequest](err); ok {
+			writeError(w, http.StatusBadRequest, err.Message)
+			return
+		}
+
+		slog.Error("registering new account: ", "error", err)
+		writeError(w, http.StatusInternalServerError, "internal server error")
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(user)
 }
