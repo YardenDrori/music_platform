89d860c0ba1523b53e8a570b87c50cd1726c2952
YardenDrori <yarden.drori.yd@gmail.com>
2026-05-21 17:28:47 +0300

    added types

---
+++ frontend/website/src/types/auth.ts
@@ -0,0 +1,14 @@
+import type { User } from "./user";
+
+export type RegisterRequest = {
+  email: string;
+  userName: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+};
+
+export type AuthResponse = {
+  user: User;
+  accessToken: string;
+};
---
+++ frontend/website/src/types/user.ts
@@ -0,0 +1,8 @@
+export type User = {
+  id: string;
+  email: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  createdAt: string;
+};
