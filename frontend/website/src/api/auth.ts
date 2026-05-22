1f1010754b30d381605061e8c442670b169753bb
YardenDrori <yarden.drori.yd@gmail.com>
2026-05-22 17:16:21 +0300

    made auth request return cleaner errors and not detail 500s to user

--- frontend/website/src/api/auth.ts
+++ frontend/website/src/api/auth.ts
@@ -1,15 +1,19 @@
 import type { AuthResponse, RegisterRequest } from "../types/auth";
 
 export async function register(req: RegisterRequest): Promise<AuthResponse> {
-  const respones = await fetch("/api/register", {
+  const response = await fetch("/api/register", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(req),
   });
 
-  const resp = await respones.json();
+  if (response.status >= 500) {
+    throw new Error("internal server error");
+  }
 
-  if (!respones.ok) {
+  const resp = await response.json();
+
+  if (!response.ok) {
     throw new Error(resp.error);
   }
 
