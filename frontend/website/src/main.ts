03e6272310f3573b0e5789c1802aec53b3bf7021
YardenDrori <yarden.drori.yd@gmail.com>
2026-05-22 21:32:09 +0300

    changed page for testing

--- frontend/website/src/main.ts
+++ frontend/website/src/main.ts
@@ -1,3 +1,4 @@
-import { renderRegister } from "./pages/register";
+import { renderLogin } from "./pages/login";
+// import { renderRegister } from "./pages/register";
 
-renderRegister();
+renderLogin();
