f72d163bda634797962f9963fb02dfbf6aee60b2
YardenDrori <yarden.drori.yd@gmail.com>
2026-05-22 21:32:47 +0300

    fixed helper

--- frontend/website/src/api/auth.ts
+++ frontend/website/src/api/auth.ts
@@ -1,4 +1,22 @@
-import type { AuthResponse, RegisterRequest } from "../types/auth";
+import type {
+  AuthResponse,
+  LoginRequest,
+  RegisterRequest,
+} from "../types/auth";
+
+async function validateResponse(resp: Response): Promise<any> {
+  if (resp.status >= 500) {
+    throw new Error("internal server error");
+  }
+
+  const respJson = await resp.json();
+
+  if (!resp.ok) {
+    throw new Error(respJson.error);
+  }
+
+  return respJson;
+}
 
 export async function register(req: RegisterRequest): Promise<AuthResponse> {
   const response = await fetch("/api/register", {
@@ -7,11 +25,8 @@ export async function register(req: RegisterRequest): Promise<AuthResponse> {
     body: JSON.stringify(req),
   });
 
-  if (response.status >= 500) {
-    throw new Error("internal server error");
-  }
-
-  const resp = await response.json();
+  return validateResponse(response);
+}
 
   if (!response.ok) {
     throw new Error(resp.error);
