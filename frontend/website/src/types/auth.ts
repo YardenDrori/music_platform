72fe6a7fa3743fcb1effd3603c6cbf1a6a8a5e81
YardenDrori <yarden.drori.yd@gmail.com>
2026-05-22 17:29:55 +0300

    added type for login request

--- frontend/website/src/types/auth.ts
+++ frontend/website/src/types/auth.ts
@@ -8,6 +8,11 @@ export type RegisterRequest = {
   password: string;
 };
 
+export type LoginRequest = {
+  identifier: string;
+  password: string;
+};
+
 export type AuthResponse = {
   user: User;
   accessToken: string;
