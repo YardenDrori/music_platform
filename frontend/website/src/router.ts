eb3ffbdc6d12fe384ffa89706285c3c65ce8d058
YardenDrori <yarden.drori.yd@gmail.com>
2026-05-23 13:10:08 +0300

    added method to validate that a user has logged in before routing

--- frontend/website/src/router.ts
+++ frontend/website/src/router.ts
@@ -1,6 +1,16 @@
 import { renderDashboard } from "./pages/dashboard";
 import { renderLogin } from "./pages/login";
 import { renderRegister } from "./pages/register";
+import { getAccessToken } from "./state";
+
+function needLogin(render: () => void) {
+  if (!getAccessToken()) {
+    history.pushState({}, "", "/login");
+  } else {
+    render();
+  }
+  route();
+}
 
 export function route() {
   const path = window.location.pathname;
@@ -10,6 +20,6 @@ export function route() {
     renderRegister();
   } else {
     //default route
-    renderDashboard();
+    needLogin(() => renderDashboard);
   }
 }
