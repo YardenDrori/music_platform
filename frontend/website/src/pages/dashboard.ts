0b53d2243623a8fc342b25501779b3a9811ba223
YardenDrori <yarden.drori.yd@gmail.com>
2026-05-27 16:58:10 +0300

    populated the dashboard with a less shitty placeholder that calls some api stuff

--- frontend/website/src/pages/dashboard.ts
+++ frontend/website/src/pages/dashboard.ts
@@ -1,5 +1,24 @@
+import { getMe } from "../api/user";
+
 export function renderDashboard(): void {
   document.getElementById("app")!.innerHTML = `
-  <p1>Placeholder - dashboard</p1>
+  <p>Fetching account information, this shouldn't take long...</p>
   `;
+
+  getMe()
+    .then((user) => {
+      console.log("own details fetched");
+      const app = document.getElementById("app")!;
+      app.innerHTML = "";
+
+      const welcomeMsg = document.createElement("p");
+      welcomeMsg.textContent = "Welcome back " + user.firstName + "!";
+
+      app.appendChild(welcomeMsg);
+    })
+    .catch((err) => {
+      const app = document.getElementById("app")!;
+      const message = err instanceof Error ? err.message : "unknown error";
+      app.textContent = "An unexpected error occurred: " + message;
+    });
 }
