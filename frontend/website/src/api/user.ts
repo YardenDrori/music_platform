39918bf3f595f912f24d5cef8a7661721b29ad0b
YardenDrori <yarden.drori.yd@gmail.com>
2026-05-28 12:50:51 +0300

    modified user methods to use the new authenticatedResponse method

--- frontend/website/src/api/user.ts
+++ frontend/website/src/api/user.ts
@@ -1,42 +1,24 @@
-import { getAccessToken } from "../state";
-import { validateEmptyResponse, validateResponse } from "../utils";
+import { authenticatedFetch } from "../utils";
 import type { User } from "../types/user";
 
 export async function getMe(): Promise<User> {
-  const access = getAccessToken();
-
-  const resp = await fetch("/api/me", {
+  return authenticatedFetch("/api/me", {
     method: "GET",
-    headers: {
-      Authorization: `Bearer ${access}`,
-    },
   });
-
-  return await validateResponse(resp);
 }
 
 export async function patchMe(modifiedUser: User) {
-  const access = getAccessToken();
-
-  const resp = await fetch("/api/me", {
+  return authenticatedFetch("/api/me", {
     method: "PATCH",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${access}`,
     },
     body: JSON.stringify(modifiedUser),
   });
-
-  return await validateEmptyResponse(resp);
 }
 
 export async function deleteMe() {
-  const access = getAccessToken();
-
-  const resp = await fetch("/api/me", {
+  return authenticatedFetch("/api/me", {
     method: "DELETE",
-    headers: { Authirzation: `Bearer ${access}` },
   });
-
-  return await validateEmptyResponse(resp);
 }
