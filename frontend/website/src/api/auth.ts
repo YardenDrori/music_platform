bba27828967a111525ae68a18bdc2f2c0f7696af
YardenDrori <yarden.drori.yd@gmail.com>
2026-05-28 12:47:24 +0300

    added the api for renewing an access token

--- frontend/website/src/api/auth.ts
+++ frontend/website/src/api/auth.ts
@@ -1,3 +1,4 @@
+import { setAccessToken } from "../state";
 import type {
   AuthResponse,
   LoginRequest,
@@ -23,4 +24,21 @@ export async function login(req: LoginRequest): Promise<AuthResponse> {
   });
 
   return validateResponse(response);
+}
+
+export async function renewAccessToken(): Promise<void> {
+  const resp = await fetch("/api/token", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  try {
+    const body = await validateResponse(resp);
+    if (!body?.accessToken) {
+      throw new Error("Didn't recieve access token from server");
+    }
+    setAccessToken(body.accessToken);
+  } catch (e) {
+    throw new Error("renewing access token", { cause: e });
+  }
 }
