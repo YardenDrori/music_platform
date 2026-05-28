8df17a3c05cb4c1972254b88e773f3d02748493d
YardenDrori <yarden.drori.yd@gmail.com>
2026-05-28 11:23:30 +0300

    improved tests

--- backend/internal/user/service_test.go
+++ backend/internal/user/service_test.go
@@ -2,16 +2,24 @@ package user
 
 import (
 	"context"
+	"fmt"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/google/uuid"
 )
 
-type mockRepo struct{}
+type mockRepo struct {
+	createFn         func(ctx context.Context, u *User) error
+	findByEmailFn    func(ctx context.Context, email string) (*User, error)
+	findByUsernameFn func(ctx context.Context, username string) (*User, error)
+	findByIDFn       func(ctx context.Context, id uuid.UUID) (*User, error)
+}
 
 func (r *mockRepo) Create(ctx context.Context, u *User) error {
+	if r.createFn != nil {
+		return r.createFn(ctx, u)
+	}
 	return nil
 }
 
@@ -24,66 +32,81 @@ func (r *mockRepo) Delete(ctx context.Context, id uuid.UUID) error {
 }
 
 func (r *mockRepo) FindByEmail(ctx context.Context, email string) (*User, error) {
-	return &User{
-		ID:           uuid.New(),
-		Email:        "test@example.com",
-		Username:     "testuser",
-		FirstName:    "Test",
-		LastName:     "User",
-		PasswordHash: strings.Repeat("a", 32),
-		CreatedAt:    time.Now(),
-		LastUpdated:  time.Now(),
-	}, nil
+	if r.findByEmailFn != nil {
+		return r.findByEmailFn(ctx, email)
+	}
+	return nil, ErrNotFound
 }
 
 func (r *mockRepo) FindByID(ctx context.Context, id uuid.UUID) (*User, error) {
-
-	return &User{
-		ID:           uuid.New(),
-		Email:        "test@example.com",
-		Username:     "testuser",
-		FirstName:    "Test",
-		LastName:     "User",
-		PasswordHash: strings.Repeat("a", 32),
-		CreatedAt:    time.Now(),
-		LastUpdated:  time.Now(),
-	}, nil
+	if r.findByIDFn != nil {
+		return r.findByIDFn(ctx, id)
+	}
+	return nil, ErrNotFound
 }
 
 func (r *mockRepo) FindByUsername(ctx context.Context, username string) (*User, error) {
-	return &User{
-		ID:           uuid.New(),
-		Email:        "test@example.com",
-		Username:     "testuser",
-		FirstName:    "Test",
-		LastName:     "User",
-		PasswordHash: strings.Repeat("a", 32),
-		CreatedAt:    time.Now(),
-		LastUpdated:  time.Now(),
-	}, nil
+	if r.findByUsernameFn != nil {
+		return r.findByUsernameFn(ctx, username)
+	}
+	return nil, ErrNotFound
 }
 
 func TestNewAccount(t *testing.T) {
 	tests := []struct {
 		name    string
 		req     *NewUserRequest
+		repo    *mockRepo
 		wantErr bool
-	}{{
-		name: "valid request",
-		req: &NewUserRequest{
-			ID:        uuid.New(),
-			Email:     "valid@email.com",
-			Username:  "testuser",
-			FirstName: "Test",
-			LastName:  "User",
-			Password:  "superdupersecretpassword",
+	}{
+		{
+			name: "happy",
+			req: &NewUserRequest{
+				ID:        uuid.New(),
+				Email:     "valid@email.com",
+				Username:  "testuser",
+				FirstName: "Test",
+				LastName:  "User",
+				Password:  "superdupersecretpassword",
+			},
+			repo:    &mockRepo{},
+			wantErr: false,
 		},
-		wantErr: false,
-	}}
+		{
+			name: "db err on create",
+			req: &NewUserRequest{
+				ID:        uuid.New(),
+				Email:     "valid@email.com",
+				Username:  "testuser",
+				FirstName: "Test",
+				LastName:  "User",
+				Password:  "superdupersecretpassword",
+			},
+			repo: &mockRepo{
+				createFn: func(ctx context.Context, u *User) error {
+					return fmt.Errorf("db is literally on fire HELP!")
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "password too short",
+			req: &NewUserRequest{
+				ID:        uuid.New(),
+				Email:     "valid@email.com",
+				Username:  "testuser",
+				FirstName: "Test",
+				LastName:  "User",
+				Password:  "shorty",
+			},
+			repo:    &mockRepo{},
+			wantErr: false,
+		},
+	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			service := NewService(&mockRepo{}, NewArgon2idPasswordHasher())
+			service := NewService(tt.repo, NewArgon2idPasswordHasher())
 			_, err := service.NewAccount(context.Background(), tt.req)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("got err = %v, wantErr = %v, request = %v", err, tt.wantErr, tt.req)
