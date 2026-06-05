44b08dd0a3c5bb0abe17a2b7f34812d4622daa8b
YardenDrori <yarden.drori.yd@gmail.com>
2026-06-05 23:31:13 +0300

    implemented minio service for the storage
    broooooooo this was a nightmare so much reading through source code all
    because the devs didnt wanna implement a method for it so i had to rad
    fucking AWS docs on how to do this shit
    
    anywho to whoever is reading this, I hope you have a wonderful day and
    goodnight from me! XD

--- backend/internal/storage/service_impl.go
+++ backend/internal/storage/service_impl.go
@@ -3,13 +3,12 @@ package storage
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"net/url"
+	"strconv"
+	"time"
 
-	"github.com/google/uuid"
 	"github.com/minio/minio-go/v7"
-	"github.com/minio/minio-go/v7/pkg/s3utils"
-
-	"github.com/YardenDrori/music-platform/internal/apperrors"
 )
 
 type service struct {
@@ -20,12 +19,89 @@ func NewService(client *minio.Client) Service {
 	return &service{s3Core: &minio.Core{Client: client}}
 }
 
-func (s *service) InitiateMutlipartPresignedUpload(
+func (s *service) InitiateMultipartUpload(
 	ctx context.Context,
 	bucketName string,
 	objectKey string,
-) (string error) {
-	s.s3Core.NewMultipartUpload()
+	opts minio.PutObjectOptions,
+) (string, error) {
+	uploadID, err := s.s3Core.NewMultipartUpload(ctx, bucketName, objectKey, opts)
+	if err != nil {
+		return "", fmt.Errorf("initiating new presigned multipart upload: %w", err)
+	}
+	return uploadID, nil
+}
 
-	panic("")
+func (s *service) GetPresignedMultipartPartsURLs(
+	ctx context.Context,
+	bucketName string,
+	objectKey string,
+	uploadID string,
+	totalPartsCount int,
+) ([]string, error) {
+	var presignedURLs []string
+	for idx := range totalPartsCount {
+		req := url.Values{
+			"partNumber": {strconv.Itoa(idx + 1)},
+			"uploadId":   {uploadID},
+		}
+		url, err := s.s3Core.Presign(
+			ctx,
+			http.MethodPut,
+			bucketName,
+			objectKey,
+			time.Duration(15)*time.Minute,
+			req,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("generating presigned multipart upload URLs: %w", err)
+		}
+		presignedURLs = append(presignedURLs, url.String())
+	}
+
+	return presignedURLs, nil
+}
+
+func (s *service) CompleteMultipartUpload(
+	ctx context.Context,
+	bucketName string,
+	objectKey string,
+	uploadID string,
+	ETags []minio.CompletePart,
+	opts minio.PutObjectOptions,
+) error {
+	_, err := s.s3Core.CompleteMultipartUpload(
+		ctx,
+		bucketName,
+		objectKey,
+		uploadID,
+		ETags,
+		opts,
+	)
+	if err != nil {
+		anotherErr := s.AbortMultipartUpload(context.Background(), bucketName, objectKey, uploadID)
+		if anotherErr != nil {
+			return fmt.Errorf(
+				"completing presgined multipart upload: %w %w",
+				err,
+				anotherErr,
+			)
+		}
+		return fmt.Errorf("completing presgined multipart upload: %w", err)
+	}
+
+	return nil
+}
+
+func (s *service) AbortMultipartUpload(
+	ctx context.Context,
+	bucketName string,
+	objectKey string,
+	uploadID string,
+) error {
+	err := s.s3Core.AbortMultipartUpload(ctx, bucketName, objectKey, uploadID)
+	if err != nil {
+		return fmt.Errorf("aborting multipart upload %w", err)
+	}
+	return nil
 }
