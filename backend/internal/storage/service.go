38f8069d923a42c6db1f432ac218838bad8baa91
YardenDrori <yarden.drori.yd@gmail.com>
2026-06-06 08:16:39 +0300

    added interface for minio storage

--- backend/internal/storage/service.go
+++ backend/internal/storage/service.go
@@ -1,3 +1,48 @@
 package storage
 
-type Service interface{}
+import (
+	"context"
+
+	"github.com/minio/minio-go/v7"
+)
+
+type Service interface {
+	//errors:
+	//fmt
+	InitiateMultipartUpload(
+		ctx context.Context,
+		bucketName string,
+		objectKey string,
+		opts minio.PutObjectOptions,
+	) (string, error)
+
+	//errors:
+	//fmt
+	GetPresignedMultipartPartsURLs(
+		ctx context.Context,
+		bucketName string,
+		objectKey string,
+		uploadID string,
+		totalPartsCount int,
+	) ([]string, error)
+
+	//errors:
+	//fmt
+	CompleteMultipartUpload(
+		ctx context.Context,
+		bucketName string,
+		objectKey string,
+		uploadID string,
+		ETags []minio.CompletePart,
+		opts minio.PutObjectOptions,
+	) error
+
+	//errors:
+	//fmt
+	AbortMultipartUpload(
+		ctx context.Context,
+		bucketName string,
+		objectKey string,
+		uploadID string,
+	) error
+}
