0df1dd99fecc5474eda6c6a5ce36800b234a56e1
YardenDrori <yarden.drori.yd@gmail.com>
2026-06-07 08:30:36 +0300

    added DTO and a helper to convert from dto to model

---
+++ backend/internal/artists/dto.go
@@ -0,0 +1,47 @@
+package artists
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type NewArtistReq struct {
+	Name             string     `json:"name"`
+	Description      *string    `json:"description"`
+	LinkToYouTube    *string    `json:"linkToYouTube"`
+	LinkToSpotify    *string    `json:"linkToSpotify"`
+	LinkToAppleMusic *string    `json:"linkToAppleMusic"`
+	BirthDate        *time.Time `json:"birthDate"`
+	BirthPlace       *string    `json:"birthPlace"`
+}
+
+type UpdateArtistReq struct {
+	ID               uuid.UUID
+	Name             *string    `json:"name"`
+	Description      *string    `json:"description"`
+	LinkToYouTube    *string    `json:"linkToYouTube"`
+	LinkToSpotify    *string    `json:"linkToSpotify"`
+	LinkToAppleMusic *string    `json:"linkToAppleMusic"`
+	BirthDate        *time.Time `json:"birthDate"`
+	BirthPlace       *string    `json:"birthPlace"`
+	DeletedAt        *time.Time `json:"-"`
+}
+
+func (a *NewArtistReq) ToArtist() Artist {
+	return Artist{
+		ID:               uuid.New(),
+		Name:             a.Name,
+		Description:      a.Description,
+		ArtistImageKey:   nil,
+		ArtistBannerKey:  nil,
+		LinkToYouTube:    a.LinkToYouTube,
+		LinkToSpotify:    a.LinkToSpotify,
+		LinkToAppleMusic: a.LinkToAppleMusic,
+		BirthDate:        a.BirthDate,
+		BirthPlace:       a.BirthPlace,
+		AddedAt:          time.Now(),
+		UpdatedAt:        time.Now(),
+		DeletedAt:        nil,
+	}
+}
