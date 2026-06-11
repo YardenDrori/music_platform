b9da71c96fe7d86f2539d2110bd39e62b87b4c7b
YardenDrori <yarden.drori.yd@gmail.com>
2026-06-12 01:05:03 +0300

    updated model added entity version of it as well as seperated the contributors part to be able to put multiple values there and allow the front to get pfps ad presigned urls

--- backend/internal/artists/model.go
+++ backend/internal/artists/model.go
@@ -1,24 +1,62 @@
 package artists
 
 import (
+	"net/url"
 	"time"
 
 	"github.com/google/uuid"
 )
 
 type Artist struct {
-	ID               uuid.UUID  `json:"id"`
-	Name             string     `json:"name"`
-	Description      *string    `json:"description"`
-	ArtistImageKey   *uuid.UUID `json:"-"`
-	ArtistBannerKey  *uuid.UUID `json:"-"`
-	LinkToYouTube    *string    `json:"linkToYouTube"`
-	LinkToSpotify    *string    `json:"linkToSpotify"`
-	LinkToAppleMusic *string    `json:"linkToAppleMusic"`
-	BirthDate        *time.Time `json:"birthDate"`
-	BirthPlace       *string    `json:"birthPlace"`
-	AddedAt          time.Time  `json:"addedAt"`
-	UpdatedAt        time.Time  `json:"updatedAt"`
-	DeletedAt        *time.Time `json:"-"`
-	ContributorIDs   []uuid.UUID `json:"contributorIds"`
+	ID               uuid.UUID      `json:"id"`
+	Name             string         `json:"name"`
+	Aliases          []string       `json:"aliases"`
+	Description      *string        `json:"description"`
+	IsBand           bool           `json:"isBand"`
+	ArtistImageKey   *uuid.UUID     `json:"-"`
+	ArtistBannerKey  *uuid.UUID     `json:"-"`
+	LinkToYouTube    *string        `json:"linkToYouTube"`
+	LinkToSpotify    *string        `json:"linkToSpotify"`
+	LinkToAppleMusic *string        `json:"linkToAppleMusic"`
+	OriginDate       *time.Time     `json:"birthDate"`
+	OriginPlace      *string        `json:"birthPlace"`
+	AddedAt          time.Time      `json:"addedAt"`
+	UpdatedAt        time.Time      `json:"updatedAt"`
+	DeletedAt        *time.Time     `json:"-"`
+	Contributions    []Contribution `json:"contributions"`
+}
+
+type ArtistEntity struct {
+	Artist
+	ContributionsEntity []ContributionEntity
+}
+
+// db entity
+type ContributionEntity struct {
+	ContributorID         uuid.UUID
+	ContributorName       string
+	ContributorProfileKey *uuid.UUID
+	ContributionDate      time.Time
+}
+
+// model+dto
+type Contribution struct {
+	ContributorID         uuid.UUID `json:"contributorId"`
+	ContributorName       string    `json:"contributorName"`
+	ContributorProfileUrl *string   `json:"contributorProfileUrl"`
+	ContributionDate      time.Time `json:"contributionDate"`
+}
+
+func (c *ContributionEntity) ToModel(profilePicUrl *url.URL) Contribution {
+	var picUrl *string
+	if profilePicUrl != nil {
+		s := profilePicUrl.String()
+		picUrl = &s
+	}
+	return Contribution{
+		ContributorID:         c.ContributorID,
+		ContributorName:       c.ContributorName,
+		ContributorProfileUrl: picUrl,
+		ContributionDate:      c.ContributionDate,
+	}
 }
