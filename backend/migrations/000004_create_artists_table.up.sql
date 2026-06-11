fc0e192e0adc880efd729fc7e9689da00fc0a08c
YardenDrori <yarden.drori.yd@gmail.com>
2026-06-11 08:42:07 +0300

    added aliases field to artists, is band field for frontend rendering birth vs formation date and added contribution date to junction table

--- backend/migrations/000004_create_artists_table.up.sql
+++ backend/migrations/000004_create_artists_table.up.sql
@@ -1,21 +1,24 @@
 CREATE TABLE artists(
   id UUID PRIMARY KEY,
   name TEXT NOT NULL,
+  aliases TEXT[],
   description TEXT,
+  is_band BOOLEAN NOT NULL,
   artist_image_key UUID,
   artist_banner_key UUID,
   link_to_youtube TEXT,
   link_to_spotify TEXT,
   link_to_apple_music TEXT,
-  birth_date DATE,
-  birth_placeCHAR(2),
+  origin_date DATE,
+  origin_place CHAR(2),
   added_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  deleted_at TIMESTAMPTZ,
+  deleted_at TIMESTAMPTZ
 );
 
 CREATE TABLE artist_contributors (
   artist_id UUID REFERENCES artists(id) ON DELETE CASCADE,
   user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+  contributed_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   PRIMARY KEY (artist_id, user_id)
 );
