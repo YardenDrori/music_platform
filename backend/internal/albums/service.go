2f837bc97d877b546a075db720bfc621e6b10c37
YardenDrori <yarden.drori.yd@gmail.com>
2026-06-13 13:17:40 +0300

    added service interface

--- backend/internal/albums/service.go
+++ backend/internal/albums/service.go
@@ -13,3 +13,17 @@ type Repository interface {
 	UpdateAlbum(ctx context.Context, req *UpdateAlbumRequest) error
 	DeleteAlbum(ctx context.Context, id uuid.UUID) error
 }
+
+type Service interface {
+	NewAlbum(ctx context.Context, req *NewAlbumRequest) error
+
+	GetAlbumsByName(ctx context.Context, name string) ([]Album, error)
+	GetAlbumByID(ctx context.Context, id uuid.UUID) (*Album, error)
+
+	UpdateAlbumDetails(ctx context.Context, req *UpdateAlbumRequest) error
+
+	SoftDeleteAlbum(ctx context.Context, id uuid.UUID) error
+	HardDeleteAlbum(ctx context.Context, id uuid.UUID) error
+
+	UploadAlbumPicture(ctx context.Context, file []byte, artistID uuid.UUID) error
+}
