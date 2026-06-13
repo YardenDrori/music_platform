df044a4202b5067bb392993327fdef882a15a9b3
YardenDrori <yarden.drori.yd@gmail.com>
2026-06-13 13:16:39 +0300

    added json tags, new artist req dto and a method to convert it to model quickly

--- backend/internal/albums/dto.go
+++ backend/internal/albums/dto.go
@@ -6,17 +6,43 @@ import (
 	"github.com/google/uuid"
 )
 
+type NewAlbumRequest struct {
+	Name         string      `json:"name"`
+	Description  *string     `json:"description"`
+	MainArtistID uuid.UUID   `json:"mainArtistID"`
+	HasAllTracks bool        `json:"hasAllTracks"`
+	PremieredAt  *time.Time  `json:"premieredAt"`
+	Artists      []uuid.UUID `json:"artists"`
+}
+
+func (r *NewAlbumRequest) ToAlbum() *Album {
+	return &Album{
+		ID:           uuid.New(),
+		Name:         r.Name,
+		Description:  r.Description,
+		MainArtistID: r.MainArtistID,
+		AlbumArtKey:  nil,
+		HasAllTracks: r.HasAllTracks,
+		AddedAt:      time.Now().UTC(),
+		UpdatedAt:    time.Now().UTC(),
+		DeletedAt:    nil,
+		PremieredAt:  r.PremieredAt,
+		Artists:      r.Artists,
+		Contributors: nil,
+	}
+}
+
 type UpdateAlbumRequest struct {
-	ID                     uuid.UUID
-	Name                   *string
-	Description            *string
-	MainArtistID           *uuid.UUID
-	AlbumArtKey            *uuid.UUID
-	HasAllTracks           *bool
-	DeletedAt              *time.Time
-	PremieredAt            *time.Time
-	ArtistsToAdd           []uuid.UUID
-	ArtistsToRemove        []uuid.UUID
-	ContributorIDsToAdd    []uuid.UUID
-	ContributorIDsToRemove []uuid.UUID
+	ID                     uuid.UUID   `json:"id"`
+	Name                   *string     `json:"name"`
+	Description            *string     `json:"description"`
+	MainArtistID           *uuid.UUID  `json:"mainArtistId"`
+	AlbumArtKey            *uuid.UUID  `json:"albumArtKey"`
+	HasAllTracks           *bool       `json:"hasAllTracks"`
+	DeletedAt              *time.Time  `json:"-"`
+	PremieredAt            *time.Time  `json:"premieredAt"`
+	ArtistsToAdd           []uuid.UUID `json:"artistsToAdd"`
+	ArtistsToRemove        []uuid.UUID `json:"artistsToRemove"`
+	ContributorIDsToAdd    []uuid.UUID `json:"-"`
+	ContributorIDsToRemove []uuid.UUID `json:"-"`
 }
