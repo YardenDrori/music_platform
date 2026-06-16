86815b1033e975e87ca0c256713bd18c24dd620c
YardenDrori <yarden.drori.yd@gmail.com>
2026-06-16 15:38:06 +0300

    refactor(migrations): merged all user migrations to one

--- backend/migrations/000001_create_users_table.up.sql
+++ backend/migrations/000001_create_users_table.up.sql
@@ -6,6 +6,22 @@ CREATE TABLE users (
     last_name     TEXT NOT NULL,
     profile_pic_key UUID UNIQUE,
     password_hash TEXT NOT NULL,
-    created_at    TIMESTAMPTZ NOT NULL,
-    is_active     BOOLEAN NOT NULL DEFAULT TRUE
+    created_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMPTZ
 );
+
+CREATE VIEW active_users AS
+SELECT * FROM users WHERE deleted_at IS NULL
+
+CREATE OR REPLACE FUNCTION set_last_updated()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW() AT TIME ZONE 'UTC';
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER users_set_last_updated
+BEFORE UPDATE ON users
+FOR EACH ROW EXECUTE FUNCTION set_last_updated();
--- backend/migrations/000003_add_last_updated_to_users.down.sql
+++
@@ -1,3 +0,0 @@
-DROP TRIGGER IF EXISTS users_set_last_updated ON users;
-DROP FUNCTION IF EXISTS set_last_updated;
-ALTER TABLE users DROP COLUMN last_updated;
