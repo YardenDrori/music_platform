c8e2ab1eadcf3aca207ef82105396c44f1ba7045
YardenDrori <yarden.drori.yd@gmail.com>
2026-06-16 23:44:49 +0300

    slimmed up albums summary

--- backend/internal/albums/model.go
+++ backend/internal/albums/model.go
@@ -22,11 +22,9 @@ type Album struct {
 }
 
 type AlbumSummary struct {
-	ID           uuid.UUID   `json:"id"`
-	Name         string      `json:"name"`
-	AlbumArtUrl  *string     `json:"albumArtUrl"`
-	MainArtistID *uuid.UUID  `json:"mainArtistId"`
-	Artists      []uuid.UUID `json:"artists"`
+	ID          uuid.UUID  `json:"id"`
+	Name        string     `json:"name"`
+	AlbumArtKey *uuid.UUID `json:"albumArtUrl"`
 }
 
 type Contributor struct {
