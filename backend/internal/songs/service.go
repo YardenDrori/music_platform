f91a5febbcb513fd42ad9d3b4bd678ad8446bb52
YardenDrori <yarden.drori.yd@gmail.com>
2026-07-04 19:08:43 +0300

    fix(): i forgot to update the repo interface for quite a bit lol sowwy

--- backend/internal/songs/service.go
+++ backend/internal/songs/service.go
@@ -11,9 +11,20 @@ import (
 type repository interface {
 	NewSong(ctx context.Context, song NewSong) error
 	GetSongByID(ctx context.Context, id uuid.UUID) (*SongRow, error)
-	GetSongSummariesByName(ctx context.Context, songName string, limit int) ([]summaries.SongSummary, error)
+	GetSongSummariesByName(
+		ctx context.Context,
+		songName string,
+		limit int,
+	) ([]summaries.SongSummary, error)
 	UpdateSong(ctx context.Context, req *UpdateSongReq) (*SongRow, error)
 	DeleteSong(ctx context.Context, id uuid.UUID) (*SongRow, error)
+	NewStagingSong(ctx context.Context, objectKey string, uploadID string, userID uuid.UUID) error
+	StagingSongOwnerFromObjectKey(ctx context.Context, uploadID string) (uuid.UUID, error)
+	DeleteStagingSong(ctx context.Context, objectKey string) error
+	EvictStagingSongs(
+		ctx context.Context,
+		abort func(objectKey string, uploadID string) error,
+	) error
 }
 
 type Service interface
